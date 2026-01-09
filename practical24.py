d4612cfbcda4901a5a99a3d09c26576b19170a95
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:59:45 +0530

    Add files via upload

---
+++ practical24.py
@@ -0,0 +1,9 @@
+#factorical
+def factorial(n):
+    if n ==0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    num = int(input("enter number:"))
+    print("factorical of ",num,"is:",factorical(num))
+    
