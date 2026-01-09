2efe044e7298a2dfcf6940a47b5da30dd27e9752
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:56:10 +0530

    Add files via upload

---
+++ practical15.py
@@ -0,0 +1,110 @@
+# set add method
+
+# x= {10,20,70,30}
+
+# x.add("aipa")
+# x.update(["sakshi","shriya","shobha"])
+# print(x)
+
+
+#dictionary
+
+# student =("key":"value")
+
+# student = {"name":"sakshi","age":20,"phone":12345678910}
+# print(student["name"])
+# print(student["age"])
+# print(student["phone"])
+
+
+#nested dictionary
+
+# student ={"AIPA":{"name":"Shailesh","age":20,"Trade":"AIPA","Address":"Teliyarganj"},
+#     "COPA":{"name":"Raj","age":20,"Trade":"COPA","Address":"Teliyarganj"},
+#     "EM":{"name":"Yash","age":20,"Trade":"EM","Address":"Teliyarganj"}
+# }
+
+# # print(student["em"]["address"])
+
+# # copy dictionary
+# students2= student.copy()
+
+# print(students2)
+
+# #clear dictionary
+
+# student.clear()
+# print("student")
+
+#dictionary methods
+
+# student={"name":"sakshi","age":20,"trade":"aipa"}
+# print(student["age"])
+# print(student["name"])
+# print(student["trade"])
+
+#ADDING NEW KEY VALUE
+# student["marks"] = 90
+# student["address"] = "Teliyarganj"
+# print(student)
+
+#REMOVING FROM DICTIONARY
+# student.pop("age")
+# print(student)
+
+
+
+# student ={"AIPA":{"name":"Shailesh","age":20,"Trade":"AIPA","Address":"Teliyarganj"},
+#     "COPA":{"name":"Raj","age":20,"Trade":"COPA","Address":"Teliyarganj"},
+#     "EM":{"name":"Yash","age":20,"Trade":"EM","Address":"Teliyarganj"}
+# }
+
+# print(student["em"]["address"])
+
+# copy dictionary
+# students2= student.copy()
+
+# print(students2)
+
+#clear dictionary
+
+# student.clear()
+# print("student")
+
+# print(student["em"]["address"])
+
+# copy dictionary
+# students2= student.copy()
+
+# print(students2)
+
+# #clear dictionary
+
+# student.clear()
+# print("student")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
