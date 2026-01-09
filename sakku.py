b746796febd22b3295ce13e970fd945dde53218b
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 15:01:21 +0530

    Add files via upload

---
+++ sakku.py
@@ -0,0 +1,29 @@
+# # creating a tuple
+
+# fruits =("apple","banana","cherry","kiwi","elderberry")
+# print(fruits)
+# number =(15,19,20,21,22)
+# print("first element:",number[0])
+# print("last element:",number[-1])
+
+# slicing
+# colours =("yellow","green","blue","red","black")
+# print("middle three colours:")
+# print(colours[2:5])
+
+# concatenation
+# x =(1,2,3)
+# y =(4,5,6)
+# z =x+y
+# print(z)
+
+# iterating
+# animal =("cow","dog","cat","lion","tiger")
+# print(animal)
+
+# membership test in tuple
+
+
+
+
+
