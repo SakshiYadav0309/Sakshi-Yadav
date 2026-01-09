6a88feb2e6ecce18438eb97a5dfc10fd291dc04d
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:57:23 +0530

    Add files via upload

---
+++ practical19.py
@@ -0,0 +1,49 @@
+
+
+# def student_details(**kwargs):
+#     for key, values in kwargs.items():
+#         print(key,":",values)
+# student_details(name = "sakshi", age = 20, course ="python", city = "prayagraj")
+
+  
+
+  # positional arguments
+   
+def mixed(nsti, opening=1992,**details)
+    print(nsti)
+    print(opening)
+
+if trade:
+     print("trade in nsti :",trade)
+     if details:
+          for key,values in details,items():
+               print(key,values)
+mix("allahabad",2000,"aipa","csa","dm","copa",phone=12345678910,email="z1z1z@example.com")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
