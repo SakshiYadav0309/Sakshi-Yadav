f7c27011d53cd69e631bc95a517cec554042944d
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:55:38 +0530

    Add files via upload

---
+++ practical 17.py
@@ -0,0 +1,37 @@
+student ={"priti","shobha","sakshi","shriya","sneha"}
+trade ={"aipa"}
+subject ={"maths","physics","chemistry","english","hindi"}
+marks ={
+    "shobha":{56,55,56,52,53},
+    "sakshi":{54,63,54,85,63},
+    "shriya":{54,63,54,85,63},
+    "sneha":{54,63,54,85,63},
+    "priti":{54,63,54,85,63}
+}
+print(student)
+print(trade)
+print(subject)
+print(marks)
+marks={
+    "shobha":{"maths":56,"physics":55,"chemistry":56,"english":52,"hindi":53},
+    "sakshi":{"maths":54,"physics":63,"chemistry":54,"english":85,"hindi":63},
+    "shriya":{"maths":54,"physics":63,"chemistry":54,"english":85,"hindi":63},
+    "sneha":{"maths":54,"physics":63,"chemistry":54,"english":85,"hindi":63},
+    "priti":{"maths":54,"physics":63,"chemistry":54,"english":85,"hindi":63}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
