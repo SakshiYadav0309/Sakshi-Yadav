6dcd6d6d4b844820debf771ef46066f6959d3e95
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:58:04 +0530

    Add files via upload

---
+++ practical20.py
@@ -0,0 +1,124 @@
+# variable scope 
+
+# college ="NSTIW"
+
+# def globals():
+#     college ="allahabad"
+#     print(college)
+
+# globals()
+# print(college)
+
+
+# name = "shobha"
+
+# def xyz():
+#     trade = "aipa"
+#     print(trade)
+
+# xyz()
+# print(name)
+
+
+# Global Scope
+
+# name = "shobha"
+
+# def gk():
+#     global name
+#     name = "shriya"
+#     print(name)
+
+# gk()
+# print(name)
+
+ 
+# Local Variable
+
+# def function1():
+#     x =10
+#     print("value in function 1:",x)
+
+# def function2():
+#     x =20
+#     print("value in function 2:",x)
+
+# function1()
+# function2()
+ 
+import os
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
