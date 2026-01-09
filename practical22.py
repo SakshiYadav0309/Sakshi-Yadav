59fc48265dc321f3c1e0325bf446083ac27ca8d5
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:59:03 +0530

    Add files via upload

---
+++ practical22.py
@@ -0,0 +1,34 @@
+# object oriented programming
+
+# class student:
+#     def __init__(sef,name,trade):
+#     self.name = name
+#     self.trade = trade 
+#     s1 = student("anamika","aipa")
+#     s2 = student("shreya","csa")
+#    print(s1.name,s1.trade)
+
+
+class college:
+    def __init__(self,college.name,fees):
+        self.college.name = college.name
+        self.fees = fees
+
+c1 = college("nstiw",1000)
+print(c1.college.name,c1.fees)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
