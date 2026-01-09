05c23b3f5255026f8f752eff18679b94c944fe69
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:56:46 +0530

    Add files via upload

---
+++ practical16.py
@@ -0,0 +1,45 @@
+#creating a dictionary
+
+student ={'name':'sakshi','age':20,'grade':'a'}
+print(student)
+
+#accessing dictionary
+
+student ={'name':'sakshi','age':20,'grade':'a'}
+print("name:",student['name'])
+print("grade:",student['grade'])
+
+# adding a new value
+
+student ={'name':'sakshi','age':20}
+student['course']='python'
+print(student)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
