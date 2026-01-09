531e758fb6ad5c3fcc5e8b6ffb99d1ed4a6ad550
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:43:50 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,14 @@
+print ("hello")
+
+print("Hello world")
+
+print("shailesh") #in-line print 
+
+
+print("shailesh \n yadav") #next line print
+
+print("""SDSFCDSC absbdkxcjanjcn sjkndx,mx nckdc
+      adclkjidlsamjl;kmcx;dasc
+      alkdjnclkjasm;oxlkcds;c
+
+""") #multi line print
