302b6832f294ca429655366fdac4a0990068d06b
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:54:12 +0530

    Add files via upload

---
+++ num.py
@@ -0,0 +1,35 @@
+
+
+
+# students =("shobha","sakshi","shriya","sneha")
+
+# for shobha in students:
+#     if shobha == "sakshi":
+#         continue
+#     if shobha == "shriya":
+#         break
+#     print(shobha)
+
+
+trade =("dm","aipa","cosmetology","copa","hotel management")
+# print(trade)
+for i in trade:
+#     if i == "dm":
+#         continue
+#     print (i)
+    # print("copa trade skipped")
+    if i == "copa":
+        break
+    print(i)
+print("end of condition")
+
+
+
+
+
+
+
+
+
+
+
