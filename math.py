257040ca94865b8cd0c137b20bd3a106b5052b3c
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:53:17 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,18 @@
+print("the sum of 10 & 20 is:",10+20)
+print("the multiplication of 63 & 46:",36*49)
+print (f"sum of {5} + {3} is {5+3}")
+f=9
+g=13
+print("the sum of f & g is:",f+g)
+h=6
+u=9
+y=10
+print("the sum of h & u & y is:",h+u+y)
+k=5
+r=10
+o=10
+a=k+o 
+b=k-o
+c=r*o
+print("the sum of",k,"and",o,"is",a)
+
