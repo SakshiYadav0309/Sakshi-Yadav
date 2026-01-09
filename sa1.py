859408b87401a95fddd1a099a0912ddd44457b0d
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 15:01:00 +0530

    Add files via upload

---
+++ sa1.py
@@ -0,0 +1,137 @@
+# institute=input("enter your institute name:")
+
+# if institute=="nstiw":
+#     trade=input("enter your trade:")
+#     if trade=="aipa":
+#         print("lab no.16")
+#     elif trade=="copa":
+#         print("lab no.19")
+#     elif trade=="dm":
+#         print("lab no.20")
+#     elif trade=="cosmetologi":
+#         print("lab no.7")
+#     else:
+#         print("enter valid trade.")
+# else:
+#     print("not a student on nstiw")
+
+# # number checker
+# num = 5
+
+# if num> 0:
+#     print("the number is positive.")
+# else:
+#     if num< 0:
+#         print("the number is negetive.")
+#     else:
+#         print("the number is zero.")
+
+
+# # age classifier
+# age = 20
+# if age>=18:
+#     print("you are a senior citizen.")
+# else:
+#     if age>=12:
+#         print("you are an adult.")
+#     else:
+#         print("you are a teenager.")
+       
+
+# exam result checker
+
+# marks = int(input("enter your marks:"))
+# attendence = int(input("enter your attendence (in %):"))
+
+# if marks >= 40:
+#     if attendence >= 75:
+#         print('pass')
+#     else:
+#         print('pass but low attendence warning')
+# else:
+#     print('fail')
+
+
+#  # voting eligibility 
+
+# age = int(input("enter persons age:"))
+# if age >= 18:
+#    citizenship = input("are you an indian citizen? (yes?/no?):")
+#    if citizenship.lower()=='yes':
+#        print ('eligible to vote in india')
+#    else:
+#        print('not eligible due to citizenship')
+# else:
+#     print('not eligible due to age')
+
+
+# # student grade
+
+# marks = float(input("enter student's marks:"))
+# if marks >= 40:
+#     if marks >= 90:
+#         print('excellent')
+#     elif marks >= 75:
+#         print('very good')
+#     else:
+#         print('good')
+# else:
+#     print('fail')
+
+
+# movie ticket discount
+
+# age = int(input("enter age:"))
+# day = input("enter day (monday to sunday):")
+# if age >= 12:
+#     if day.lower() =='sunday':
+#         print('free ticket for kids!')
+#     else:
+#         print('half price ticket ')
+# else:
+#     print('no discount available')
+
+
+
+# loop concept
+
+# Name = "shriya"
+# for i in range(5):
+#     print (Name)
+
+# for i in range(6,12):
+#     print (1)
+
+num = 20
+for i in range(1,11):
+        print (num,"x",i,"=",num*i)
+else:
+    print ("table completed:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
