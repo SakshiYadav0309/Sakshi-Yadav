7b203275723a6050f75bdadf5f6ab5104f1783d7
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:53:37 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,88 @@
+# # # Inbuilt moduls
+
+# # import math
+
+# # print (math. sqrt(9))
+# # print (math. pi)
+# # print (math.pow(3,4))
+# # print(math.factorial(5))
+
+
+# import datetime
+
+# now = datetime.datetime.now()
+# today = datetime.date.today()
+# future = today + datetime.timedelta(days=10)
+# print (today)
+# print (now)
+# print (now.year)
+# print (now.month)
+# print (now.day)
+
+
+# import sys
+
+# print (sys.version)
+# print (sys.executable)
+# print (sys.platform)
+
+
+# import random
+
+# print (random.randint(1,10))
+# print (random.random())
+# print (random.choice(["aipa","csa","copa"]))
+# trade = [("aipa","copa","em","dm")]
+# random.shuffle(trade)
+# print(trade)
+
+
+# import os
+
+# print (os.getcwd())
+# print (os.listdir())
+# # os.mkdir("aipa")
+# os.rmdir("aipa")
+
+# import time
+
+# print (time.ctime())
+# time.sleep(10)
+# print ("after 5 seconds")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
