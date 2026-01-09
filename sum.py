341ae7bfece146cbe5c60193a29f23088edc64df
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 15:01:40 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,43 @@
+# while loop
+
+# num = 5
+# while num > 0:
+#     print(num)
+#     num -= 1
+# print("loop finished")
+
+# money = 0
+
+# while money >1000:
+#     money +=50
+#     print("added money:",money)
+# print("contribution done Rs.1000")
+
+import time
+num = 5
+while num > 0:
+    print(num)
+    num -= 1
+    time.sleep(2)
+print("loop finished")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
