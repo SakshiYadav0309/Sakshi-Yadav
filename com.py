82c5562054cb26f850ad094c1e71408f92f0fc2c
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:49:39 +0530

    Add files via upload

---
+++ com.py
@@ -0,0 +1,90 @@
+# # loop concept
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(5):
+#     print("sakshi")
+
+# for i in range(2,21,2):
+#     print(i)
+
+# for i in range(2,21):
+#     if i % 2 == 0:
+#         print(i)
+
+# for i in range(1,20,2):
+#     print(i)
+# for i in range(1,20):
+#     if i % 2 != 0:
+#         print(i)
+        
+# for i in range(1,11):
+#     print(i**2)
+
+# num = int(input("enter a number:"))
+# for i in range(1,11):
+#     print(f"{num} x {i} = {num * i}")
+
+# for i in range(10,0, -1):
+#     print(i)
+
+sum_of_numbers = 0
+for i in range(1,11):
+    sum_of_numbers += i
+    print(sum_of_numbers)
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
