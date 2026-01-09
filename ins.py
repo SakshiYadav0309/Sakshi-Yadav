29d0f56a1ba6e34fe7b258bb95c85e1b72b044c8
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:52:19 +0530

    Add files via upload

---
+++ ins.py
@@ -0,0 +1,73 @@
+# assigning integer literals to variables
+a = 10
+b = -5
+c = 0
+
+print("integer values:")
+print("a =",a)
+print("b =",b)
+print("c =",c)
+
+#performing operations on integer literals
+print("sum of a and b:",a+b)
+print("product of and c:",a*c)
+print("difference between a and b:",a-b)
+
+#assigning floating point literals to variables
+a = 3.14
+b = -2.718
+c = 0.0
+
+#performing operation with floating-point numbers
+sum_float = a+b
+product_float = a*b
+differente_float = a-b
+
+#printing floating-point values and rules of operations
+print("\nfloating-point values:")
+print("a =",a)
+print("b =",b)
+print("c =",c)
+print("sum of a and b:", sum_float)
+print("product of a and b:",product_float)
+print("difference between a and b:",differente_float)
+
+
+#assigning complex literals to variables
+a = 2+3j
+b = -1 -4j
+c = 0 + 0j
+
+#performing operation with complex numbers
+sum_complex = a+b
+product_complex = a*b
+differente_complex = a-b
+
+#printing complex values and rules of operations
+print("\ncomplex values:")
+print("a =",a)
+print("b =",b)
+print("c =",c)
+print("sum of a and b:", sum_complex)
+print("product of a and b:",product_complex)
+print("difference between a and b:",differente_complex)
+
+#single-queoted strings
+single_quoted = 'hello,world!'
+
+# string operations with single-quoted strings
+length = len(single_quoted) # length of the sting
+upper_case = single_quoted.upper() # convert to uppercase
+lower_case = single_quoted.lower() # convert to lowercase
+contains_substring = 'world' in single_quoted # check if substring is present
+
+#print results
+print("single-quoted string:", single_quoted)
+print("length of the string:", length)
+print("uppercase version:", upper_case)
+print("lowercase version:", lower_case)
+print("contains 'world':", contains_substring)
+
+
+
+
