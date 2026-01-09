b8203e6cf1ee16473aa3b43a739a039437eeeb66
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:51:45 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,44 @@
+# name="sakshi"
+# age=20
+# phone=12345678910
+# year=2022
+# print(type(name))
+# print(type(age))
+# print(type(phone))
+# print(type(year))
+
+
+
+
+x=6
+y=3
+print(x>y)
+
+x=3
+y=9
+
+print(x<y)
+print(x>y)
+name="sakshi"
+age=20
+print(name+age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
