3f5511a091a810d9b11838213d2da4e5dad2edb0
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:51:21 +0530

    Add files via upload

---
+++ da1.py
@@ -0,0 +1,38 @@
+# abc=[1,3,"sakshi","sneha","neha","charu"]
+# print(abc[2:4])
+# abc.append("aipa")
+
+# creating a list
+fruits =["apple","banana","cherry","kiwi","mango"]
+print("original list:",fruits)
+
+# accesing elements list
+first_fruit = fruits[0] 
+last_fruit = fruits[-1]
+print("first fruit:",first_fruit)
+print("last fruit:",last_fruit)
+
+# slicing a list
+sublist_fruits = fruits[:3]
+print("sublist of first three fruits:", sublist_fruits)
+
+# adding elements to list
+fruits.append("grape")
+print("list after adding 'grape':", fruits)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
