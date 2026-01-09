cf5c9b2e301ed5727ab1cfda2e85860a8588db90
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:52:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+# # conditional statement if elif else 
+
+# weather = "breezy"
+
+# if weather == "sunny":
+#     print(" take sunglasses")
+
+# elif weather == "rainy":
+#     print(" take umbrella")
+
+# else:
+#     print("stay at home")
+
+
+# vehicle=input("enter your vehicle name:")
+
+# if vehicle in ("xuv","swift","benz","suzuki","honda"):
+#     print("its a four wheeler")
+# elif vehicle in ("scooty","bajaj","yamaha","apache"):
+#     print("its a two wheeler")
+# else:
+#      print("unknown vehicle")
+
+
+language = "python"
+
+if language>=input("python"):
+    print("its a programming language")
+elif language>=input("java"):
+    print("its a programming language")
+else:
+    print("unknown ")            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
