abff6dcd582903b970e0274c9b3572d1776c8619
SakshiYadav0309 <sakku.yadav2390@gmail.com>
2026-01-09 14:58:42 +0530

    Add files via upload

---
+++ practical21.py
@@ -0,0 +1,33 @@
+
+# import os
+# os.mkdir("shobha")
+
+# Rename Dirctory
+# os.rename("shobha","csa")
+
+# remove dirctory
+# os.rmdir("csa")
+
+# # rename all files
+# import forloop1
+# forloop1.rmtree("forloop")
+
+# list all files in folder
+# import os
+# xyz = os.listdir(".")
+# print(xyz)
+
+# python = os.listdir()
+# print(python)(".")
+
+
+
+
+
+
+
+
+
+
+
+
